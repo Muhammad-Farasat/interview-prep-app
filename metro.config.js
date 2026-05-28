dd037d5ac68e9939b56e5f6c8b5db9a112a34017
Muhammad-Farasat <farasatkhan687@gmail.com>
2026-05-28 17:25:01 +0500

    fix: downgraded expo to stable version

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.unstable_enablePackageExports = false;
+// Needed for Clerk
+config.resolver.unstable_enablePackageExports = true;
+
+// Needed for Supabase Realtime
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'ws' || moduleName === 'stream') {
+    return { type: 'empty' };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
